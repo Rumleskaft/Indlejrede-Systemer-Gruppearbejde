142bc19a57eaa9ec3b410fdbaaf3eeb6a9112181
Rumleskaft <Danielblyme@gmail.com>
2014-09-17 15:02:41 +0200

    merge fix

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -12,31 +12,16 @@
 int outputArray[30], ptr = 0;
 
 int main() {
-<<<<<<< HEAD
-	// floop
-
-	int input = getNextData();
-	printf("Before: %d%s", input, " ");
-	int output = filt(input);
-	printf("After: %d\n", output);
-	outputArray[ptr] = output;
-
-	/*peakDetection()
-    	outputArraoutput.push(output)*/
-	loopCheck(ptr++, sizeof(outputArray));
-
-	return 0;
-=======
     // floop
     int i;
     for(i=0; i<100; i++){
         int x = getNextData();
         int y = filt(x);
-        yArray[0] = y;
+        outputArray[0] = y;
     }
     /*peakDetection()
     yArray.push(y)*/
     
     return 0;
->>>>>>> origin/master
+
 }
